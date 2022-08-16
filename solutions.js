0c45ce26730b0b2aedafe50bcd1e849b13c52c0b
Exabiier Bars <Exabiier@yahoo.com>
2022-08-16 09:31:42 -0500

    create gitignore

--- solutions.js
+++ solutions.js
@@ -20,3 +20,45 @@
  * and
  * concat
  */
+function isTrue(entry){
+    return entry === true;
+}
+
+function isFalse(entry1) {
+    entry1 === false;
+    }
+
+
+function not(entry3){
+    return !entry3;
+}
+
+function addOne(entry4){
+    let x = parseFloat(entry4);
+    return x + 1;
+}
+
+function isEven(entry5){
+    return entry5 % 2 === 0;}
+
+function isIdentical(value1, value2){
+    return value1 === value2;
+}
+
+function isEqual(v1, v2){
+    return v1 == v2;}
+
+function or(v1, v2){
+    return v1 || v2;
+}
+
+function and(v1, v2){
+    return v1 || v2;
+}
+
+function concat(v1, v2){
+    return toString(v1) + toString(v2);
+}
+
+
+
